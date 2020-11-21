4f3920050e94d6f0803eb412dcafed985d8b270c
gio8tisu <gio8.tisu@gmail.com>
2020-11-21 20:58:31 +0100

    ADD caller Dockerfile
    First test to call workers.

---
+++ celery-worker/caller_entrypoint.py
@@ -0,0 +1,22 @@
+import time
+import argparse
+
+from test_celery import tasks
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--times", type=int, default=1)
+
+    args = parser.parse_args()
+
+    for _ in range(args.times):
+        result = tasks.longtime_add.delay(1, 2)
+        # at this time, our task is not finished, so it will return False
+        print("Task finished?", result.ready())
+        print("Task result:", result.result)
+        # sleep 10 seconds to ensure the task has been finished
+        res = result.get(timeout=10)
+        # now the task should be finished and ready method will return True
+        print("Task finished?", result.ready())
+        print("Task result:", res)

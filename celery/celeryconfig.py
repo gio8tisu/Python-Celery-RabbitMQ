0086fe8bff96c47d31de43209086e3017775288a
gio8tisu <gio8.tisu@gmail.com>
2020-11-22 14:21:46 +0100

    Remove all services but broker

--- celery/celeryconfig.py
+++
@@ -1,9 +0,0 @@
-import os
-broker_url = os.environ.get("CELERY_BROKER_URL", "amqp://");
-result_backend = "mongodb://results:27017/"
-mongodb_backend_settings = {
-    "database": "celery-results",
-    "user": "admin",
-    "password": "password",
-    "taskmeta_collection": "my_taskmeta_collection",
-}

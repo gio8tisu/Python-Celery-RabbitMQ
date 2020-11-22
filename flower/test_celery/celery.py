156e53de5db48413b256a6156648b42cc8c3ad2f
gio8tisu <gio8.tisu@gmail.com>
2020-11-22 14:14:52 +0100

    refactor: mount test_celery directory instead of having 3 copies

--- flower/test_celery/celery.py
+++
@@ -1,9 +0,0 @@
-import celery
-
-
-app = celery.Celery("test_celery",
-                    include=["test_celery.tasks"])
-app.config_from_object("celeryconfig")
-
-if __name__ == "__main__":
-    app.start()

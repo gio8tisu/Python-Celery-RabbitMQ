4d7691ce7796c893a91859dedc470ab4d64af088
Sergio Garcia <garcia90@clinic.cat>
2020-11-23 13:28:57 +0100

    Remove erroneus comments

--- caller_entrypoint.py
+++ caller_entrypoint.py
@@ -17,8 +17,6 @@ if __name__ == "__main__":
         # at this time, our task is not finished, so it will return False
         print("Task finished?", result.ready())
         print("Task result:", result.result)
-        # sleep 10 seconds to ensure the task has been finished
         res = result.get(timeout=10)
-        # now the task should be finished and ready method will return True
         print("Task finished?", result.ready())
         print("Task result:", res)
